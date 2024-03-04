4bd9abbd68f2e0de4810512700143d1b3eab87d3
TOMERUE <feriyusgiantoro@gmail.com>
2024-03-04 18:45:57 +0700

    Create serigala1.java

---
+++ serigala1.java
@@ -0,0 +1,25 @@
+public class serigala1 {
+    public String nama = "Bowo";
+    public int umur = 10;
+    public String ras = "Husky";
+    public String makanan = "Daging";
+    public String warna = "Abu";
+
+    public void displaySerigalala() {
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+        System.out.println("Ras : " + ras);
+        System.out.println("Makanan: " + makanan);
+        System.out.println("Warna : " + warna);
+
+    }
+
+    public void nama() {
+        System.out.println("Nama : " + nama);
+    }
+
+    public void ras() {
+        System.out.println("Ras : " + ras);
+    }
+
+}
